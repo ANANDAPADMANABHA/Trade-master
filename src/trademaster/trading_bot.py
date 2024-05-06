837ac8d08cdd0738133f738528450a85187c0fcd
ygag-ananthu <118436967+ygag-ananthu@users.noreply.github.com>
2024-05-06 20:35:33 +0530

    Trade bot implemented

---
+++ src/trademaster/trading_bot.py
@@ -0,0 +1,28 @@
+import time
+import pandas as pd
+import datetime as dt
+from data_loader import ORB_TICKERS
+from strategies.opening_range_breakout import OpeningRangeBreakout
+
+class TradeMaster(OpeningRangeBreakout):
+    def make_some_money(self) -> None:
+            starttime = time.time()
+            hi_lo_prices = {}
+            self._load_instrument_list()
+            self._initialize_smart_api()
+            data_0920 = self.hist_data_0920(
+                ORB_TICKERS, 4, "FIVE_MINUTE", self.instrument_list)
+
+            for ticker in ORB_TICKERS:
+                hi_lo_prices[ticker] = [data_0920[ticker]["high"].iloc[-1], data_0920[ticker]["low"].iloc[-1]]
+            time.sleep(300 - ((time.time() - starttime) % 300.0))
+
+            while dt.datetime.now() < dt.datetime.strptime(dt.datetime.now().strftime('%Y-%m-%d')+' 14:30', '%Y-%m-%d %H:%M'):
+                print("starting passthrough at {}".format(dt.datetime.now()))
+                positions = pd.DataFrame(self.smart_api.position()["data"])
+                open_orders = self.get_open_orders()
+                self.orb_strat(ORB_TICKERS, hi_lo_prices, positions, open_orders)
+                time.sleep(300 - ((time.time() - starttime) % 300.0))
+                
+trade = TradeMaster()
+trade.make_some_money()
