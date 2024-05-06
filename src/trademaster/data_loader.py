739462fc3942798bd9be0569784a8079b82c711e
ygag-ananthu <118436967+ygag-ananthu@users.noreply.github.com>
2024-05-06 20:32:52 +0530

    Data loder constants added

---
+++ src/trademaster/data_loader.py
@@ -0,0 +1,4 @@
+ORB_TICKERS = ['POWERGRID','SBIN','TATASTEEL','HINDALCO','UPL','WIPRO','NTPC','COALINDIA']
+#TODO write down different list of stocks based on the strategies they work on and use it from here
+#eg : if one of the orb stock is not working as expected or a new stock need to be added it should be added here
+YRB_TICKERS = ['POWERGRID','SBIN','TATASTEEL','HINDALCO','UPL','WIPRO','NTPC','COALINDIA']
